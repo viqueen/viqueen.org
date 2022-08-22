1ef7d67590340a5f77757128f39a1b2257225569
Hasnae <hasnae@viqueen.org>
2022-08-22 17:57:48 +1000

    welcome youtube widget

--- src/site/content/extensions/WidgetConnectorMacro.tsx
+++ src/site/content/extensions/WidgetConnectorMacro.tsx
@@ -6,16 +6,12 @@ interface WidgetConnectorMacroProps {
 
 const SPOTIFY_TRACK =
     /^https:\/\/open\.spotify\.com\/track\/(?<trackId>[a-zA-Z0-9]+)(\?.*)?$/;
+const YOUTUBE_VIDEO =
+    /^https:\/\/www\.youtube\.com\/watch\?v=(?<videoId>[\w]+)$/;
 
-export default function WidgetConnectorMacro({
-    url
-}: WidgetConnectorMacroProps) {
-    const matcher = url.match(SPOTIFY_TRACK);
-    if (!matcher) return <></>;
-    const trackId = matcher.groups?.trackId;
+const spotifyEmbed = (trackId?: string) => {
     if (!trackId) return <></>;
-
-    const embedUrl = `https://open.spotify.com/embed/track/${trackId}?`;
+    const embedUrl = `https://open.spotify.com/embed/track/${trackId}`;
     return (
         <div style={{ textAlign: 'center' }}>
             <iframe
@@ -29,4 +25,34 @@ export default function WidgetConnectorMacro({
             />
         </div>
     );
+};
+
+const youtubeEmbed = (videoId?: string) => {
+    if (!videoId) return <></>;
+    const embedUrl = `https://www.youtube.com/embed/${videoId}`;
+    return (
+        <div style={{ textAlign: 'center' }}>
+            <iframe
+                width="560"
+                height="315"
+                src={embedUrl}
+                title="YouTube video player"
+                frameBorder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                allowFullScreen={false}
+            />
+        </div>
+    );
+};
+
+export default function WidgetConnectorMacro({
+    url
+}: WidgetConnectorMacroProps) {
+    const spotify = url.match(SPOTIFY_TRACK);
+    if (spotify) return spotifyEmbed(spotify.groups?.trackId);
+
+    const youtube = url.match(YOUTUBE_VIDEO);
+    if (youtube) return youtubeEmbed(youtube.groups?.videoId);
+
+    return <></>;
 }
